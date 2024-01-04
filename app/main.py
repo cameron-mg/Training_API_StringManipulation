e1518740d5fc87c2c934529053861d684acfcbe5
Cameron Mackay-Grant <cameronmackaygrant@gmail.com>
2024-01-04 01:08:27 +0000

    Finished text compute function

--- app/main.py
+++ app/main.py
@@ -7,3 +7,36 @@ app = FastAPI()
 @app.get('/')
 def root():
     return {"Hello" : "World"}
+
+
+def lengthCalc(filename):
+    import string
+    # Opening and reading file in 
+    f = open(filename, "r")
+    text = f.read()
+
+    # Cleaning all punctuation out of the string and splitting words
+    punctuationToClean = string.punctuation.replace("-","").replace("'","").replace("&","").replace("/","")
+    cleanText = text.translate(str.maketrans("", "", punctuationToClean)).split() # removes punctuation and splits on whitespace
+
+    # Declaring counting vars
+    lengths = [0 for i in range(45)] # define array used to store length values
+    count = 0 # total word count
+    charcount = 0
+
+    # Looping through clean text, counting and averaging
+    for word in cleanText:
+        wordLength = len(word)
+        count += 1 # output: word count
+        charcount += wordLength
+        lengths[wordLength] += 1 # output: array of word length counts
+
+    averageWordLength = charcount / count # output: average length of words
+    mostFrequentCount = max(lengths) # output: most frequent word lengths
+    mostFrequentCountLengths = [index for index, value in enumerate(lengths) if value == mostFrequentCount] # output: word lengths of most frequent lengths
+
+    return {"Word_Count" : count, 
+            "Avg_Length" : averageWordLength, 
+            "Length_Array": lengths, 
+            "Length_Mode": mostFrequentCount, 
+            "Length_Mode_Length": mostFrequentCountLengths}
