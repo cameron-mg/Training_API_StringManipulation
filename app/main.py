29e69a5487bf4b1c840ad0fa0134263336833c94
Cameron Mackay-Grant <cameronmackaygrant@gmail.com>
2024-01-05 02:02:39 +0000

    Added test file and upload file endpoint

--- app/main.py
+++ app/main.py
@@ -1,24 +1,27 @@
 # docker build -t dominimage .
 # docker run -d --name containerapi -p 80:80 dominimage
-from fastapi import FastAPI  
+from fastapi import FastAPI, File, UploadFile
+import re
 
 app = FastAPI()
 
 @app.get('/')
 def root():
-    return {"Hello" : "World"}
+    return {"Root" : "Hello"}
+
+@app.post('/upload/')
+async def upload(file: UploadFile = File(...)):
+    text = await file.read()
+    return lengthCalc(str(text))
 
 
-def lengthCalc(filename):
+def lengthCalc(text):
     import string
-    # Opening and reading file in 
-    f = open(filename, "r")
-    text = f.read()
 
-    # Cleaning all punctuation out of the string and splitting words
+    # Cleaning all whitespace characters and punctuation out of the string and splitting words
+    # cleanText = re.sub(r"\\n", "", text)
     punctuationToClean = string.punctuation.replace("-","").replace("'","").replace("&","").replace("/","")
     cleanText = text.translate(str.maketrans("", "", punctuationToClean)).split() # removes punctuation and splits on whitespace
-
     # Declaring counting vars
     lengths = [0 for i in range(45)] # define array used to store length values
     count = 0 # total word count
---
+++ test.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'http://127.0.0.1:8000/upload/'
+files = {'file': open(r'C:\Users\camer\Documents\Coding_Misc\DominAPI\text.txt', 'r')}
+
+response = requests.post(url, files=files)
+
+print(response.text)
