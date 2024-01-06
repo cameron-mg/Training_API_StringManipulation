5e243eae98fd00cdd09d0f0632e6aca8fd8241e6
Cameron Mackay-Grant <cameronmackaygrant@gmail.com>
2024-01-06 03:10:59 +0000

    Updated required packages and changed main function to use dicts

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # docker build -t dominimage .
 # docker run -d --name containerapi -p 80:80 dominimage
 from fastapi import FastAPI, File, UploadFile
+# from fastapi_cache2 import *
 import re
 
 app = FastAPI()
@@ -20,10 +21,10 @@ def lengthCalc(text):
 
     # Cleaning all whitespace characters and punctuation out of the string and splitting words
     # cleanText = re.sub(r"\\n", "", text)
-    punctuationToClean = string.punctuation.replace("-","").replace("'","").replace("&","").replace("/","")
+    punctuationToClean = string.punctuation.replace("-","").replace("'","").replace("&","")
     cleanText = text.translate(str.maketrans("", "", punctuationToClean)).split() # removes punctuation and splits on whitespace
     # Declaring counting vars
-    lengths = [0 for i in range(45)] # define array used to store length values
+    lengths = {} # define dictionary used to store length values
     count = 0 # total word count
     charcount = 0
 
@@ -32,11 +33,14 @@ def lengthCalc(text):
         wordLength = len(word)
         count += 1 # output: word count
         charcount += wordLength
-        lengths[wordLength] += 1 # output: array of word length counts
+        if wordLength in lengths.keys():
+            lengths[wordLength] += 1 # output: array of word length counts
+        else:
+            lengths[wordLength] = 1
 
     averageWordLength = charcount / count # output: average length of words
-    mostFrequentCount = max(lengths) # output: most frequent word lengths
-    mostFrequentCountLengths = [index for index, value in enumerate(lengths) if value == mostFrequentCount] # output: word lengths of most frequent lengths
+    mostFrequentCount = max(lengths.values()) # output: most frequent word lengths
+    mostFrequentCountLengths = [key for key, value in lengths.items() if value == mostFrequentCount] # output: word lengths of most frequent lengths
 
     return {"Word_Count" : count, 
             "Avg_Length" : averageWordLength, 
