07634b6c3bd27d6e5626245f6b2e58ce91dadaa0
Cameron Mackay-Grant <cameronmackaygrant@gmail.com>
2024-01-07 03:55:57 +0000

    Test script to run all files in test folder and show execution times

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# url = 'http://127.0.0.1:80/upload/' # docker image url
-url = 'http://127.0.0.1:8000/upload/' # uvicorn local test url
-files = {'file': open(r'C:\Users\camer\Documents\Coding_Misc\DominAPI\test_text_files\text.txt', 'r')}
-
-response = requests.post(url, files=files)
-
-print(response.text)
---
+++ testexe/test.py
@@ -0,0 +1,26 @@
+import requests
+import time
+import os
+
+# files = {'file': open(r'C:\Users\camer\Documents\Coding_Misc\DominAPI\testexe\test_text_files\text.txt', 'r')}
+
+# url = 'http://127.0.0.1:80/upload/' # docker image url
+url = 'http://127.0.0.1:8000/upload/' # uvicorn local test url
+directory = './test_text_files'
+
+for filename in os.listdir(directory): # looping through files in test directory
+    for i in range(2): # loop twice to check once without cache and once with
+
+        files = {'file' : open(os.path.join(directory, filename), 'r')}
+        
+        start = time.time()
+        response = requests.post(url, files=files) # API call
+        end = time.time()
+
+        match i:
+            case 0:
+                nocachetime = end-start
+            case 1:
+                cachetime = end-start
+
+    print(f"For file: {filename}, caching saved: {(nocachetime-cachetime)*1000:.03f}ms")
