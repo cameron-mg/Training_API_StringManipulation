894518786c5cc6a4331498209184adc37d99f6bb
Cameron Mackay-Grant <cameronmackaygrant@gmail.com>
2024-01-07 19:13:22 +0000

    Completed string output formatting and changed test file to test 2x length mode

--- run.py
+++ run.py
@@ -9,10 +9,14 @@ try:
     files = {'file': open(f'{filepath}', 'r')}
     response = requests.post(url, files=files) # API call
     data = response.json()
+
     print(f"\nWord count = {data['Word_Count']} \nAverage word length = {data['Avg_Length']:.03f}")
+
     for key in data['Length_Array']:
         value = data['Length_Array'][key]
-        print(key, value)
+        print(f"The number of words of length {key} is {value}.")
+
+    print(f"The most frequently occuring word length is {data['Length_Mode']}, for word lengths of {data['Length_Mode_Length']}")
 
 except Exception as e:
     print(e)
